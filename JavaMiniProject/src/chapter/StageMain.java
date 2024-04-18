3ef95cd52b44395d1ed4718062c8294c9a2c0df8
smhrd <smhrd@DESKTOP-26J3GME>
2024-04-18 11:30:09 +0900

    Merge branch 'master' of https://github.com/2024-SMHRD-SW-DataDegign-1/EscapeGame 게임 부분 메소드

---
+++ JavaMiniProject/src/chapter/StageMain.java
@@ -0,0 +1,12 @@
+package chapter;
+
+public class StageMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Chap0 c0 = new Chap0();
+		
+		c0.choo0();
+	}
+
+}
--- JavaMiniProject/src/javaMiniProject/StageChoose.java
+++ JavaMiniProject/src/javaMiniProject/StageChoose.java
@@ -27,34 +27,7 @@ public class StageChoose {
 		System.out.print("어디로 둘러볼까? >> ");
 		int choose = sc.nextInt();
 
-		if (choose == 1)
-		{
-			// if choose 1번선택지...
-			// 1번 구역으로 접근 시 2가지의 선택지가 다시 출력
-			System.out.println("해당 근처의 컴퓨터로 갔으나 모니터는 비밀번호가 걸어져있다. 힌트도 있으며 내용은 다음과 같았다.");
-			System.out.println(" ");
-			System.out.println("악마의 숫자를 의미하는 3자리 수를 입력하라. 단 숫자는 전부 동일하다.");
-			// 정답 : 666
 
-			System.out.print("정답 입력 >> ");
-			int answer1 = sc.nextInt();
-
-			if (answer1 == 666) {
-				// 정답일 경우 바로 첫번째 문제를 출력시킨다
-				System.out.println("정답이다. 라고 생각한 순간 말이 끝나기 무섭게 첫번째 문제가 출력되고 있다.");
-				System.out.println(" ");
-				System.out.println("1번 문제 출력");
-
-			} else {
-				System.out.println("오답이다. 재도전 : 1\t 포기한다 : 2");
-				int choose2 = sc.nextInt();
-				
-				if (choose2 == 1) {
-					
-				
-				}
-			}
-		}
 
 		if (choose == 2)
 		{
