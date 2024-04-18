0bb34616dc5c777d272ecce4adc8bbaa54f1197c
smhrd <smhrd@DESKTOP-O9LEM59>
2024-04-18 09:47:24 +0900

    LockDoor Class 추가 StartMain에 연결

--- JavaMiniProject/src/start/StartMain.java
+++ JavaMiniProject/src/start/StartMain.java
@@ -2,16 +2,21 @@ package start;
 
 import java.util.Scanner;
 
+import javaMiniProject.LockDoor;
+
 public class StartMain {
 
 	public static void main(String[] args) {
 		
 		Start str = new Start();
 		
+		LockDoor ld = new LockDoor();
+		
 		Scanner sc = new Scanner(System.in);
 		
 		str.start();
 
+		ld.lockDoor();
 
 	}
 
