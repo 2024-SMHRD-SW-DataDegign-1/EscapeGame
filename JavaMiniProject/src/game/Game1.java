b54b2f189b34c47972027ab07553f6796bdd292d
smhrd <smhrd@DESKTOP-26J3GME>
2024-04-18 15:44:25 +0900

    Merge branch 'master' of https://github.com/2024-SMHRD-SW-DataDegign-1/EscapeGame

--- JavaMiniProject/src/game/Game1.java
+++ JavaMiniProject/src/game/Game1.java
@@ -3,40 +3,50 @@ package game;
 import java.util.Scanner;
 
 public class Game1 {
-	
+
 	Scanner sc = new Scanner(System.in);
-	
+
 	public void g1() {
 		System.out.println("컴퓨터 메모장에 증감연산자를 사용한 메모가 보인다.  \n");
-		System.out.println("	int Number = 39;\r\n"
-				+ "\r\n"
-				+ "	Number;		-> 39\r\n"
-				+ "	Number++;	-> (  )\r\n"
-				+ "	++Number;	-> 41\r\n"
-				+ "	Number--;	-> (  )\r\n"
-				+ "	Number++;	-> 41\r\n"
-				+ "	Number++;	-> (  )\r\n"
-				+ "	--Number;	-> (  )\r\n"
-				+ "	++Number;	-> 42");
+//		System.out.println("	int Number = 39;\r\n"
+//				+ "\r\n"
+//				+ "	Number;		-> 39\r\n"
+//				+ "	Number++;	-> (  )\r\n"
+//				+ "	++Number;	-> 41\r\n"
+//				+ "	Number--;	-> (  )\r\n"
+//				+ "	Number++;	-> 41\r\n"
+//				+ "	Number++;	-> (  )\r\n"
+//				+ "	--Number;	-> (  )\r\n"
+//				+ "	++Number;	-> 42");
+//		
+//		
+//		System.out.println();
+//		System.out.println("빈 칸에 들어갈 숫자를 차례대로 적으시오\n");
+//		
+//		System.out.println("'빈 칸에 들어갈 숫자..?'");
+//		
+//		System.out.print("정답 >> ");
+//		String answer = sc.next();
+//		
+//		if(answer.equals("40 40 42 41")) {
+//			System.out.println("정답입니다. ");
+//		} else {
+//			System.out.println("오답입니다");
+//		}
+
+		System.out.println("		int Number = 39;\r\n"
+				+ "		\r\n"
+				+ "		Number++;\r\n"
+				+ "		++Number;\r\n"
+				+ "		Number--;\r\n"
+				+ "		Number++;\r\n"
+				+ "		Number++;\r\n"
+				+ "		--Number;\r\n"
+				+ "		++Number;	\r\n"
+				+ "		\r\n");
 		
-		System.out.println();
-		System.out.println("빈 칸에 들어갈 숫자를 차례대로 적으시오\n");
-		
-		System.out.println("'빈 칸에 들어갈 숫자..?'/n");
-		
-		System.out.print("정답 >> ");
-		String answer = sc.next();
-		
-		if(answer.equals("40 40 42 41")) {
-			System.out.println("정답입니다. ");
-		}
-		
-		
-		
-		
-		
-		//System.out.println(Number);
-		
+		System.out.println("		Number = ?");
+
 	}
 
 }
