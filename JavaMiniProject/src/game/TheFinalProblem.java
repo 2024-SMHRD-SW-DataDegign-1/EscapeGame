500384aa05a87099c1f29aa21bfe088d98adf850
smhrd <smhrd@DESKTOP-O9LEM59>
2024-04-19 16:39:37 +0900

    ZZ

--- JavaMiniProject/src/game/TheFinalProblem.java
+++ JavaMiniProject/src/game/TheFinalProblem.java
@@ -7,16 +7,16 @@ public class TheFinalProblem {
 		switch (num0) {
 		
 		case 1 :
-			System.out.println("첫번째 비밀번호는 " + num1 + "입니다.");
+			System.out.print("첫번째 비밀번호는 " + num1 + "입니다.");
 			break;
 		case 2 :
-			System.out.println("두번째 비밀번호는 " + num2 + "입니다.");
+			System.out.print("두번째 비밀번호는 " + num2 + "입니다.");
 			break;
 		case 3 :
-			System.out.println("세번째 비밀번호는 " + num3 + "입니다.");
+			System.out.print("세번째 비밀번호는 " + num3 + "입니다.");
 			break;
 		case 4 :
-			System.out.println("네번째 비밀번호는 " + num4 + "입니다.");
+			System.out.print("네번째 비밀번호는 " + num4 + "입니다.");
 			break;
 
 		}
